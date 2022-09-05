7ea49bcb489b083c4b58a63945d76068c1363267
Ankit kumar jha <ankitjha2603@gmail.com>
2022-09-05 22:31:27 +0530

    Add files via upload

---
+++ readme.md
@@ -0,0 +1,35 @@
+# Algorithm Visualizer
+This is a project to visualize algorithms using HTML, CSS, JavaScript.
+- **What is Algorithm**  
+```
+An algorithm is a set of instructions for solving a problem or accomplishing a task.
+The algorithm is the most important part of any program.
+```
+
+### Algorithm
+- Sorting Algorithm
+  - Bubble sort ___(Time complexity = О(n<sup>2</sup>))___
+  - Insertion Sort ___(Time complexity = О(n<sup>2</sup>))___
+  - Selection Sort ___(Time complexity = О(n<sup>2</sup>))___
+  - Quick sort ___(Time complexity = О(log<sub>2</sub>n)___
+  - Merge sort ___(Time complexity = О(log<sub>2</sub>n)___
+
+- Binary Search
+  - Binary search on Sorted array ___(Time complexity = О(log<sub>2</sub>n)___
+  - Binary search in infinite sorted array (constant speed) ___(Time complexity = О(log<sub>2</sub>n)___
+  - Binary search in infinite sorted array (exponential speed) ___(Time complexity = О(log<sub>2</sub>n)___
+  - Binary search on rotated sorted Array ___(Time complexity = О(log<sub>2</sub>n)___
+
+- Backtraking
+  - Sudoku solver ___(Time complexity = О(9<sup>n</sup>))___
+  - nQueen problem Solver ___(Time complexity = О(2<sup>n</sup>))___
+
+# upcoming feature or update
+- Many more algorithms
+
+# contact me
+- [Github](https://github.com/ankitjha2603)
+- [LinkedIn](https://linkedin.com/in/ankitjha2603)
+- [Stack Overflow](https://stackoverflow.com/users/17047946/ankit-kumar-jha)
+- [Instagram](https://www.instagram.com/kumarjha2603)
+- <a href = "mailto: ankitjha2603@gmail.com">Gmail</a>
---
+++ serviceWorker.js
@@ -0,0 +1,69 @@
+const staticCacheName = 'algorithm-visulizer-static-v1.1.0';
+const assets = [
+  "/algorithm-visualizer/",
+  "/algorithm-visualizer/index.html",
+
+  "/algorithm-visualizer/assets/music/move.mp3",
+  
+  "/algorithm-visualizer/assets/css/style.css",
+  "/algorithm-visualizer/assets/css/custom-right-click-menu.css",
+  "/algorithm-visualizer/assets/css/nav.css",
+  "/algorithm-visualizer/assets/css/sorting.css",
+  "/algorithm-visualizer/assets/css/sudoku.css",
+  "/algorithm-visualizer/assets/css/responsive.css",
+  "/algorithm-visualizer/assets/css/nQueen.css",
+
+  "/algorithm-visualizer/assets/js/function.js",
+  "/algorithm-visualizer/assets/js/sorting.js",
+  "/algorithm-visualizer/assets/js/binarySearch.js",
+  "/algorithm-visualizer/assets/js/backtracking.js",
+  "/algorithm-visualizer/assets/js/script.js",
+  "/algorithm-visualizer/assets/js/windowEventListener.js",
+
+  "/algorithm-visualizer/assets/img/chess-queen-svgrepo-com.svg",
+  "/algorithm-visualizer/assets/img/portfolio.svg",
+  "/algorithm-visualizer/assets/img/snake-game.svg",
+  "/algorithm-visualizer/assets/img/icons8-disney-now.svg",
+  "/algorithm-visualizer/assets/img/icons8-github.svg",
+  "/algorithm-visualizer/assets/img/icons8-instagram.svg",
+  "/algorithm-visualizer/assets/img/icons8-linkedin.svg",
+  "/algorithm-visualizer/assets/img/icons8-refresh.svg",
+  "/algorithm-visualizer/assets/img/icons8-stack-overflow.svg",
+];
+
+// install event
+self.addEventListener('install', evt => {
+  evt.waitUntil(
+    caches.open(staticCacheName).then((cache) => {
+      console.log('caching shell assets');
+      return cache.addAll(assets)
+        .catch(err => {
+          console.error('Error adding files to cache', err);
+        })
+    })
+  )
+  console.info('SW installed');
+  self.skipWaiting();
+});
+
+// activate event
+self.addEventListener('activate', evt => {
+  evt.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(keys
+        .filter(key => key !== staticCacheName)
+        .map(key => caches.delete(key))
+      );
+    })
+  );
+  return self.clients.claim();
+});
+
+// fetch event
+self.addEventListener('fetch', evt => {
+  evt.respondWith(
+    caches.match(evt.request).then(cacheRes => {
+      return cacheRes || fetch(evt.request);
+    })
+  );
+});
